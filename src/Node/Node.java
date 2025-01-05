1d8b1affbe304314272ade3b68828394c827ba13
scinteiegabriel <gabi.scinteie00@gmail.com>
2025-01-05 18:32:31 +0200

    Nodes know only the ports of their neighbors now

--- src/Node/Node.java
+++ src/Node/Node.java
@@ -9,6 +9,7 @@ public class Node {
     private int parentId = -1; // If node has parent = 0, then he is root. -1 means not assigned yet
     private List<Integer> neighbors;
     private Map<Integer, Integer> portsMap;
+    private DatagramSocket socket = null;
     private String subtree = "";
     private Set<Integer> setChildren = new HashSet<>();
     private Set<Integer> setParent = new HashSet<>();
@@ -26,23 +27,25 @@ public class Node {
         this.portsMap = new HashMap<>(portsMap);
 
         // Delete from portsMap all the nodes that aren't neighbors. A node must only know about its neighbors
-        Iterator<Map.Entry<Integer, Integer>> iterator = portsMap.entrySet().iterator();
+        Iterator<Map.Entry<Integer, Integer>> iterator = this.portsMap.entrySet().iterator();
         while (iterator.hasNext()) {
             Map.Entry<Integer, Integer> entry = iterator.next();
-            if (neighbors.contains(entry.getKey())) {
+            if (!neighbors.contains(entry.getKey())) {
                 iterator.remove();
             }
         }
 
+        System.out.println("Node: " + nodeId + " neighbors ports: " + this.portsMap);
+
         connections.addAll(neighbors);
     }
 
-
+    // TODO: WHEN RECEIVING A MESSAGE FROM SOMEONE, IF THEY ARE NEIGHBORS, ADD THEM TO PORTS MAP
 
     public void start() {
         try {
             System.out.println("Node " + nodeId + " port " + port);
-            DatagramSocket socket = new DatagramSocket(port);
+            socket = new DatagramSocket(port);
             byte[] buffer = new byte[256];
             String message;
             String senderIdString;
@@ -51,7 +54,7 @@ public class Node {
                 socket.receive(packet);
                 String fullMessage = new String(packet.getData(), 0, packet.getLength());
 
-                int senderPort = packet.getPort();  // This gives the port of the sender
+                int senderPort = packet.getPort();
                 InetAddress senderAddress = packet.getAddress();
 
                 String[] parts = fullMessage.split(":", 2);
@@ -64,10 +67,12 @@ public class Node {
                 message = parts[1];
 
                 int senderId = Integer.parseInt(senderIdString);
-                // If I'm root, add the port of the client in order to respond to him
-                if(senderId == 0){
-                    portsMap.put(0, senderPort);
+                // If the sender is not in the map of known ports, add him
+                if(!portsMap.containsKey(senderId))
+                {
+                    portsMap.put(senderId, senderPort);
                 }
+
                 System.out.println("Node " + nodeId + " received from " + senderId + " message: " + message);
                 handleMessage(senderId, message);
             }
@@ -96,22 +101,17 @@ public class Node {
         } else if(Objects.equals(message, "PARENT")) {
             setChildren.add(senderId);
 
-            if(allConnectionsResponded()) {
-                printChildren();
-            }
         } else if(Objects.equals(message, "ALREADY")) {
             setOther.add(senderId);
 
-            if(allConnectionsResponded()) {
-                printChildren();
-
-                // If every connection finished and I'm still a leaf, then I'll send a message to my parent that I'm ready for the subtree building
-                if(setChildren.isEmpty() && !done){
+            // If every connection responded and I haven't already sent DONE, then I can send done to the parent
+            if(allConnectionsResponded() && !done) {
+                // If I'm leaf, then I'll send a tree building message to my parent containing only my ID
+                if(setChildren.isEmpty()){
                     done = true;
                     sendMessage(parentId, "DONE:" + nodeId);
-
-                } else if(number_children_done == setChildren.size() && !done) {
-                    // If the node has children, but has received an already message that completes his connections list, then the node has to send its subtree to the parent
+                } else if(number_children_done == setChildren.size()) {
+                    // If I have children, and all connections responded and I haven't sent DONE to parent, then I can close the children list and send my subtree to parent
                     subtree += ")";
 
                     done = true;
@@ -129,14 +129,12 @@ public class Node {
             }
 
             // If all connections responded and the number of children who are done is equal to the total number of children
-            // We need to check that all conections responded because if not, we might send a DONE message when posibly a child hasn't responded to us yet, but he will respond with a parent message soon.
+            // We need to check that all conections responded because if not, we might send a DONE message before all the possible children responded to us
             if(number_children_done == setChildren.size() && allConnectionsResponded() && !done) {
                 subtree += ")";
 
-                // We need to validate if done is true because in the current implementation a node might send done multiple times
                 done = true;
                 sendMessage(parentId, "DONE:" + subtree);
-//                clearNode();
             }
         }
     }
@@ -223,25 +221,17 @@ public class Node {
     private void sendMessage(int neighbor, String message) {
         try {
             InetAddress address = InetAddress.getByName("localhost");
-            DatagramSocket socket = new DatagramSocket();
             String fullMessage = nodeId + ":" + message; // Include sender ID in the message
             byte[] buffer = fullMessage.getBytes();
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, portsMap.get(neighbor));
 
             // Simulate delay of message sending
             simulateDelay();
-//            if(neighbor == 5 && nodeId == 2)
-//            {
-//                try {
-//                    Thread.sleep(3000);
-//                } catch (InterruptedException e) {
-//                    System.err.println("Thread was interrupted");
-//                }
-//            }
+
             socket.send(packet);
 
-            socket.close();
             System.out.println("Node " + nodeId + " sent: " + fullMessage + " to neighbor " + neighbor);
+            System.out.println(portsMap);
         } catch (Exception e) {
             e.printStackTrace();
         }
